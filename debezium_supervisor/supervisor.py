22f95d1535f529820d4962de47b558d71bc8d3c4
lumenghe <lumenghe@gmail.com>
2020-08-06 11:12:43 +0200

    new class

--- debezium_supervisor/supervisor.py
+++ debezium_supervisor/supervisor.py
@@ -7,3 +7,14 @@ from requests.auth import HTTPBasicAuth
 
 logger = logging.getLogger(__name__)
 
+
+class Supervisor:
+    def __init__(self, kafka_config, cdc_config):
+        self._kafka_config = kafka_config
+        self._logger = logger
+        self._url = f"""https://{cdc_config}:8443/connectors"""
+        self._verify = self._kafka_config["extra_params"]["ssl.ca.location"]
+        self._cert = (
+            self._kafka_config["extra_params"]["ssl.certificate.location"],
+            self._kafka_config["extra_params"]["ssl.key.location"],
+        )
