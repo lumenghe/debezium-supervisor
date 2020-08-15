312fac758ae3d18622d85c4214b405a7b1cd562b
lumenghe <lumenghe@gmail.com>
2020-08-15 17:32:27 +0200

    new function 'main'

--- debezium_supervisor/app.py
+++ debezium_supervisor/app.py
@@ -10,3 +10,21 @@ from debezium_supervisor.supervisor import Supervisor
 
 logger = logging.getLogger(__name__)
 
+
+@click.command()
+def main():
+    with open("config.yml", "r") as ymlfile:
+        cfg = yaml.load(ymlfile)
+    try:
+
+        # handle config log
+        logging.config.dictConfig(cfg["log"])
+
+        supervisor = Supervisor(
+            kafka_config=cfg["kafka"], cdc_config=cfg["cdc"]
+        )
+        supervisor.start()
+    # reason: we want to catch all exceptions
+    except Exception as e:
+        stacktrace = traceback.format_exc()
+        logger.error("{}: {}".format(e.__class__.__name__, stacktrace))
